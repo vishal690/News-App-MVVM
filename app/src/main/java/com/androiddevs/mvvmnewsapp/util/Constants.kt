0dfaf2778268b13184a8cc2c70bdd9ae3469e01a
vishal690 <vishal690@chouhan@.com>
2022-12-22 12:48:42 +0530

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "f11916387e3248cea1facd2a11a0fbf9"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
